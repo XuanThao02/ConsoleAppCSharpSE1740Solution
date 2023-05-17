a454865f6490d6b05857f699d792800ca524d03b
Xuan Thao Nguyen <nguyenxuanthao2002@outlook.com>
2023-05-17 08:09:36 +0700

    Upload nha má, check task làm nha má

--- ConsoleAppCSharpSE1740/Program.cs
+++ ConsoleAppCSharpSE1740/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace ConsoleAppCSharpSE1740
+﻿namespace ConsoleAppCSharpSE1740.Programming;
+
+public class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        int a = 10, b = 20;
+        Console.WriteLine($"Sum = {Add(a,b)}");
+        Console.ReadKey();
     }
+    public static int Add(int x,int y) => x + y;
+    public static double Add(double x,double y) => x + y;
+    public static double Add(int x, int y, double z) => Add(Add(x, y), z);
 }
